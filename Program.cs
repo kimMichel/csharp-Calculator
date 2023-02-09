185f4a01d1015ce8cf7035699eda4ac69fd85229
kim <kmichel.jae@gmail.com>
2023-02-09 00:12:36 -0300

    Add WelcomeBoard and Get values to calculate

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WelcomeBoard();
+
+            double firstNumber = GetFirstNumber();
+            double secondNumber = GetSecondNumber();
+
+        }
+
+        static void WelcomeBoard()
+        {
+            Console.Clear();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("-----------Bem vindo-----------");
+            Console.WriteLine("-------------------------------");
+        }
+
+        static double GetFirstNumber()
+        {
+            Console.WriteLine("Digite o primeiro valor a ser calculado:");
+            var number = Console.ReadLine();
+            return Convert.ToDouble(number);
+        }
+
+        static double GetSecondNumber()
+        {
+            Console.WriteLine("Digite o segundo valor a ser calculado:");
+            var number = Console.ReadLine();
+            return Convert.ToDouble(number);
         }
     }
 }
